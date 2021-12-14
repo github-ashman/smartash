c7e21d5f32c3c596d3a31c3d1de04a8dccdce714
github-ashman <95285294+github-ashman@users.noreply.github.com>
2021-12-14 09:44:24 +0000

    Update Contract.py

--- Contract.py
+++ Contract.py
@@ -39,3 +39,14 @@ W3 = Web3(WebsocketProvider('wss://ropsten.infura.io/ws/v3/%s'%infuraKey))
 account1=Account.from_key(privateKey);
 address1=account1.address
 Greeter = W3.eth.contract(abi=abi, bytecode=bytecode)
+nonce = W3.eth.getTransactionCount(address1)
+#diagnostics
+#print(nonce)
+# Submit the transaction that deploys the contract
+tx_dict = Greeter.constructor().buildTransaction({
+'chainId': 3,
+'gas': 1400000,
+'gasPrice': w3.toWei('40', 'gwei'),
+'nonce': nonce,
+'from':address1
+})
