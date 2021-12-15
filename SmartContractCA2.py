cd2ab6414f8fcf598d5df460c83bdc2bbd66dc06
github-ashman <95285294+github-ashman@users.noreply.github.com>
2021-12-15 22:25:20 +0000

    Create SmartContractCA2.py

---
+++ SmartContractCA2.py
@@ -0,0 +1,71 @@
+import time
+import json
+import web3
+from eth_account import Account
+from web3.auto import w3
+from web3.providers.websocket import WebsocketProvider
+from web3 import Web3
+from solc import compile_standard
+with open("Contract.sol") as c:
+ contractText=c.read()
+with open(".pk") as pkfile:
+ privateKey=pkfile.read()
+with open(".infura") as infurafile:
+ infuraKey=infurafile.read()
+
+compiled_sol = compile_standard({
+     "language": "Solidity",
+     "sources": {
+         "Greeter.sol": {
+             "content": contractText
+         }
+     },
+     "settings":
+         {
+             "outputSelection": {
+                 "*": {
+                     "*": [
+                         "metadata", "evm.bytecode"
+                         , "evm.bytecode.sourceMap"
+                     ]
+                 }
+             }
+         }
+ })
+bytecode = compiled_sol['contracts']['Greeter.sol']['Greeter']['evm']['bytecode']['object']
+abi = json.loads(compiled_sol['contracts']['Greeter.sol']['Greeter']['metadata'])['output']['abi']
+W3 = Web3(WebsocketProvider('wss://ropsten.infura.io/ws/v3/%s'%infuraKey))
+account1=Account.from_key(privateKey);
+address1=account1.address
+Greeter = W3.eth.contract(abi=abi, bytecode=bytecode)
+nonce = W3.eth.getTransactionCount(address1)
+
+tx_dict = greeter.functions.setGreeting('Namaste').buildTransaction({
+'chainId': 3,
+'gas': 1400000,
+'gasPrice': w3.toWei('40', 'gwei'),
+'nonce': nonce,
+'from':address1
+})
+signed_txn = W3.eth.account.sign_transaction(tx_dict, private_key=privateKey)
+
+print("Deploying the Smart Contract")
+result = W3.eth.sendRawTransaction(signed_txn.rawTransaction)
+
+tx_receipt = None#W3.eth.getTransactionReceipt(result)
+
+count = 0
+while tx_receipt is None and (count < 300):
+  time.sleep(1)
+  try:
+    tx_receipt = W3.eth.getTransactionReceipt(result)
+  except:
+    print('.')
+
+if tx_receipt is None:
+  print (" {'status': 'failed', 'error': 'timeout'} ")
+
+print("\nContract address is:",tx_receipt.contractAddress)
+
+print("\nOutput from greet()")
+print(greeter.functions.greet().call({"from":account1.address}))
