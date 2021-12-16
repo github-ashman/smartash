d2624d7ac2bc5949e00abdb7e4e23a0b659e6f03
github-ashman <95285294+github-ashman@users.noreply.github.com>
2021-12-16 12:49:36 +0000

    Update SmartContractCA2.py

--- SmartContractCA2.py
+++ SmartContractCA2.py
@@ -40,7 +40,7 @@ address1=account1.address
 Greeter = W3.eth.contract(abi=abi, bytecode=bytecode)
 nonce = W3.eth.getTransactionCount(address1)
 
-tx_dict = greeter.functions.setGreeting('Hello Paul, this is Ashley-10584204').buildTransaction({
+tx_dict = Greeter.functions.setGreeting('Hello Paul, this is Ashley-10584204').buildTransaction({
 'chainId': 3,
 'gas': 1400000,
 'gasPrice': w3.toWei('40', 'gwei'),
@@ -71,6 +71,5 @@ greeter = W3.eth.contract(
   address=tx_receipt.contractAddress,
   abi=abi
 )
-
 print("\nOutput from greet()")
 print(greeter.functions.greet().call({"from":account1.address}))
